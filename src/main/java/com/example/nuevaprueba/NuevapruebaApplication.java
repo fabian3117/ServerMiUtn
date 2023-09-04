d8545ad3d3f0242d2687386ccb705b1ec776e940
Fede <fabian3117@frba.utn.edu.ar>
2023-09-04 18:30:01 -0300

    test

---
+++ src/main/java/com/example/nuevaprueba/NuevapruebaApplication.java
@@ -0,0 +1,13 @@
+package com.example.nuevaprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuevapruebaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NuevapruebaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nuevaprueba/NuevapruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nuevaprueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuevapruebaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
