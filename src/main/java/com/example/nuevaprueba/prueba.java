80ddabe63047948d1d76c0f10a05e1fb08ba991a
Federico Gonzalez <34560661+fabian3117@users.noreply.github.com>
2023-09-05 23:21:16 -0300

    Update prueba.java

--- src/main/java/com/example/nuevaprueba/prueba.java
+++ src/main/java/com/example/nuevaprueba/prueba.java
@@ -2,6 +2,7 @@ package com.example.nuevaprueba;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RestController
