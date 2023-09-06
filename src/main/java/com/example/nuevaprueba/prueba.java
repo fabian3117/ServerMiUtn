f39af5f4158b41f13a4168a1c3489f2a8de251a7
Federico Gonzalez <34560661+fabian3117@users.noreply.github.com>
2023-09-05 23:05:10 -0300

    Update prueba.java

--- src/main/java/com/example/nuevaprueba/prueba.java
+++ src/main/java/com/example/nuevaprueba/prueba.java
@@ -1,11 +1,13 @@
 package com.example.nuevaprueba;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/")
 @Controller
+@RestController
+@RequestMapping("/")
 public class prueba {
-@RequestMapping("")
+@GetMapping("")
     public String inicio(){
 
     return "inicio";
